1b9c9ebbc7e217777f952a4b0bdbd278590f885b
Giridhar <giridharpatel143@gmail.com>
2024-03-05 19:43:11 +0530

    Initlize new hono project
    1) Initalized new hono project
    2) Created basic routes
    3) Initalized prisma in project
    4) Established connection pooling using prisma

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('api/v1/user/signup', (c) => {
+  return c.text("hello")
+})
+app.post('api/v1/user/signin', (c) => {
+  return c.text("hello")
+})
+
+app.post('api/v1/blog', (c) => {
+  return c.text("hello")
+})
+
+app.put('api/v1/blog', (c) => {
+  return c.text("hello")
+})
+
+app.get('api/v1/blog/:id', (c) => {
+  return c.text("hello")
+})
+
+app.get('api/v1/blog/bulk', (c) => {
+  return c.text("hello")
+})
+
+export default app
