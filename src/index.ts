f6e944be12f2949a0182e1b9d6e9002ef87c41f9
Giridhar <giridharpatel143@gmail.com>
2024-03-06 23:01:41 +0530

    Database models and signup route creation
    1) Created user and post schema
    2) Added signup route wiht jwt authentication

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {decode, sign, verify} from 'hono/jwt'
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL : string,
+    JWT_SECRET : string
+  }
+}>()
 
-const app = new Hono()
+app.get('/', async (c) => {
+  
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+const body = await c.req.json();
+
+const user = await prisma.user.create({
+  data: {
+    email : body.email,
+    name : body.name,
+    password : body.password
+  },
+})
+
+const token = await sign({id : user.id},c.env.JWT_SECRET)
+
+  return c.json({
+    jwt : token
+  })
 })
 
 app.post('api/v1/user/signup', (c) => {
